1e18c0ca1278ff8b6abecefdc6afced4808c45c0
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2018-06-27 20:04:46 +0300

    fix username on Android

--- exports.zsh
+++ exports.zsh
@@ -2,6 +2,10 @@
 
 export LANG="en_US.UTF-8"
 export LC_ALL="$LANG"
+
+if [[ -z $USER && -n $USERNAME ]]; then
+  export USER="$USERNAME"
+fi
 export DEFAULT_USER=dmitmel
 
 if [[ -n $SSH_CONNECTION ]]; then
