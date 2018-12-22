ddf9b3445b15c25eee9aeab0b619f74b3c7c8cf3
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2018-12-23 00:36:10 +0200

    update upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -4,7 +4,7 @@ DOTFILES_PATH="${0:h}"
 cd "$DOTFILES_PATH" || exit 1
 
 git pull --rebase --stat origin master
-git submodule update --init --recursive --remote
+git submodule update --init --recursive --remote --progress
 
 source "./zgen/zgen.zsh"
 zgen update
