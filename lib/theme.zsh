35b801d6b9c04203ff50e259897ed1b1b222b625
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2019-01-13 16:15:55 +0200

    use bold color in prompt

--- lib/theme.zsh
+++ lib/theme.zsh
@@ -12,5 +12,5 @@ configure_dircolors
 # This ugly hack is required only for the agnoster theme which I use. I'm
 # probably going to switch to another theme because it is so damn slow
 autoload -Uz add-zsh-hook
-_patch-prompt() { PROMPT="$PROMPT"$'\n%{%F{247}%}\u03bb>%{%b%f%} '; }
+_patch-prompt() { PROMPT="$PROMPT"$'\n%{%B%}\u03bb>%{%b%} '; }
 add-zsh-hook precmd _patch-prompt
