48a4e3f8ec3471cb37f27d24bb32e6bf643f4726
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2019-01-13 15:49:08 +0200

    patch prompt a bit

--- lib/theme.zsh
+++ lib/theme.zsh
@@ -8,3 +8,9 @@ configure_dircolors() {
 }
 
 configure_dircolors
+
+# This ugly hack is required only for the agnoster theme which I use. I'm
+# probably going to switch to another theme because it is so damn slow
+autoload -Uz add-zsh-hook
+_patch-prompt() { PROMPT="$PROMPT"$'\n%{%F{247}%}\u03bb>%{%b%f%} '; }
+add-zsh-hook precmd _patch-prompt
