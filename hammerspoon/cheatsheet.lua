a838ded382738e08cddfdb5b8f8bae4f58b922f9
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2019-04-22 23:34:11 +0300

    [hammerspoon] remove cheatsheet script completely

--- hammerspoon/cheatsheet.lua
+++
@@ -1,8 +0,0 @@
-hs.loadSpoon("KSheet")
-
-modal = hs.hotkey.modal.new({"cmd", "alt", "ctrl"}, "c")
-
-function modal:entered() spoon.KSheet:show() end
-function modal:exited()  spoon.KSheet:hide() end
-
-modal:bind('', 'escape', function() modal:exit() end)
--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,4 +1,3 @@
 require "TerminalPalette"
--- require "cheatsheet"
 
 hs.notify.show("Hammerspoon", "", "Hammespoon config loaded")
