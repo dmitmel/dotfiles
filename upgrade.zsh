aeee5f485fe8b0aafd537b868a825bc00121a535
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2019-07-06 00:19:55 +0300

    fix upgrade script

--- upgrade.zsh
+++ upgrade.zsh
@@ -1,11 +1,11 @@
 #!/usr/bin/env zsh
 
-DOTFILES_PATH="${0:h}"
+DOTFILES_PATH="${0:a:h}"
 cd "$DOTFILES_PATH" || exit 1
+ZSH_DOTFILES="$DOTFILES_PATH/zsh"
 
 git pull --rebase --stat origin master
 git submodule update --init --recursive --remote --progress
 
-DISABLE_AUTO_UPDATE=true
-source "zsh/zgen/zgen.zsh"
-zgen update
+source "$ZSH_DOTFILES/plugins.zsh"
+plugin-upgrade
