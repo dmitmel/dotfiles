18ddb83d4b3dccdf2bbdca84db684b7d1dbb351d
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2019-11-14 19:40:52 +0200

    [scripts] add factors function for pycalc

--- scripts/pycalc_startup.py
+++ scripts/pycalc_startup.py
@@ -1,3 +1,14 @@
 from math import *
 
+
+def factors(n):
+    result = set()
+    for i in range(1, int(sqrt(n)) + 1):
+        div, mod = divmod(n, i)
+        if mod == 0:
+            result.add(div)
+            result.add(mod)
+    return result
+
+
 print("loaded Python calculator")
