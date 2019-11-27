6a4b9beaca9caff4472b613e21624f0ef674fb5c
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2019-11-27 09:32:23 +0200

    [scripts] fix factors function in pycalc

--- scripts/pycalc_startup.py
+++ scripts/pycalc_startup.py
@@ -5,10 +5,9 @@ from fractions import Fraction
 def factors(n):
     result = set()
     for i in range(1, int(sqrt(n)) + 1):
-        div, mod = divmod(n, i)
-        if mod == 0:
-            result.add(div)
-            result.add(mod)
+        if n % i == 0:
+            result.add(i)
+            result.add(n // i)
     return result
 
 
