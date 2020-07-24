4e42db45d62bd1cd4801652ed5a6876de1950f63
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-07-24 14:33:04 +0300

    [crosscode] upgrade the "btw I use Arch" mod to ccloader 3

--- crosscode/btw-i-use-arch-mod/preload.js
+++
@@ -1,6 +0,0 @@
-// I know that this is legacy feature provided by CCLoader way back when mods
-// weren't executed inside the game iframe's context, but it is useful
-// nevertheless because console in Chrome's devtools automatically selects
-// window.top as the execution context by default, so writing `cc.` is faster
-// than `modloader.frame.contentWindow`
-window.top.cc = window;
