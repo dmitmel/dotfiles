5a5067b98c98468ff3fb4bc4442f51a5815091fa
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-10-04 19:50:52 +0300

    [colorschemes] add a generator for xfce4-terminal

---
+++ colorschemes/xfce4-terminal.theme.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import _theme as theme
+
+print("[Scheme]")
+print("Name=dmitmel's dotfiles colorscheme")
+print("ColorForeground={}".format(theme.fg))
+print("ColorBackground={}".format(theme.bg))
+print("ColorCursorUseDefault=FALSE")
+print("ColorCursorForeground={}".format(theme.cursor_fg))
+print("ColorCursor={}".format(theme.cursor_bg))
+print("ColorSelectionUseDefault=FALSE")
+print("ColorSelection={}".format(theme.selection_fg))
+print("ColorSelectionBackground={}".format(theme.selection_bg))
+print("TabActivityColor={}".format(theme.ansi_colors[1]))
+print("ColorBoldUseDefault=TRUE")
+print("ColorPalette={}".format(";".join(theme.ansi_colors)))
