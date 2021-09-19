767b70b76b456d25d67ad7f4fa3201736c61272e
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2021-09-19 15:06:39 +0300

    [zsh] disable emojis in Homebrew

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -31,6 +31,7 @@ export JQ_COLORS="${(j.:.)jq_colors}"
 unset jq_colors
 
 export HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_NO_EMOJI=1
 
 # https://github.com/junegunn/fzf/blob/764316a53d0eb60b315f0bbcd513de58ed57a876/src/tui/tui.go#L496-L515
 export FZF_DEFAULT_OPTS="--color=16 --height=40% --reverse"
