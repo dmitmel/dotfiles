c3f7aa73519a94548923d4494c87c603be1f094b
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2021-10-04 12:49:13 +0300

    [userscripts] improve the very important script

--- web/userscripts/zno-ext-port.user.js
+++ web/userscripts/zno-ext-port.user.js
@@ -2,7 +2,7 @@
 // @name        Переглядач коментарів zno.osvita.ua
 // @description Порт додатку для Google Chrome для безкоштовного перегляду коментарів на сайті zno.osvita.ua.
 // @homepage    https://github.com/Blaumaus/zno-ext/tree/415add75513b734597ca8765bf1658b1b3bd087f
-// @version     2
+// @version     4
 // @grant       none
 // @match       https://zno.osvita.ua/*
 // @run-at      document-end
@@ -13,7 +13,23 @@
   if (!(document.referrer != null && document.referrer.includes('zno.osvita.ua'))) {
     window.location.replace(location.href);
   }
+
+  let comments = new Map();
   for (let item of document.querySelectorAll('.explanation')) {
     item.style.setProperty('display', 'inline-block', 'important');
+    comments.set(item.id, item.innerHTML);
+  }
+
+  if (window.onload != null) {
+    window.onload = null;
+  } else {
+    setInterval(() => {
+      for (let item of document.querySelectorAll('.explanation')) {
+        item.style.setProperty('display', 'inline-block', 'important');
+        if (comments.has(item.id)) {
+          item.innerHTML = comments.get(item.id);
+        }
+      }
+    }, 1000);
   }
 })();
