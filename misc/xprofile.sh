467371a96de1402e27e44336b6224c22044c2882
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2022-04-04 16:54:22 +0300

    [misc] delete xprofile.sh

--- misc/xprofile.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-export XDG_MENU_PREFIX=lxde-
-export MOZ_USE_XINPUT2=1
-export QT_QPA_PLATFORMTHEME=qt5ct
-export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true'
