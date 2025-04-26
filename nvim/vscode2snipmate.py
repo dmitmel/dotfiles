836ecd342ba8c23ab773c050b8a134a466912c7a
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2025-04-27 00:22:23 +0200

    [nvim] convert vscode snippets to snipmate syntax for now
    This was done because snipmate syntax is much easier to write and is
    more flexible (with support for `extends` and Vimscript evaluation),
    plus it is easier to work with them in LuaSnip.

---
+++ nvim/vscode2snipmate.py
@@ -0,0 +1,11 @@
+import sys
+import json5
+
+def main():
+  for _, snippet in json5.load(sys.stdin).items():
+    print(f"snippet {snippet['prefix']} {snippet.get('description', '')}")
+    for line in snippet["body"]:
+      print("\t" + line)
+
+if __name__ == "__main__":
+  main()
