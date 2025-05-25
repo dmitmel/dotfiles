f8a1ec6180f7b90d78de8edce11af7ffa9d851e0
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2025-05-25 14:22:23 +0200

    [nvim] don't hardcode Ubuntu fonts in my CSS stylesheets

--- colorschemes/out/colorscheme.lua
+++ colorschemes/out/colorscheme.lua
@@ -3,22 +3,22 @@ theme.base16_name = "eighties"
 theme.is_dark = true
 ---@type table<number, { gui: number, cterm: number, r: number, g: number, b: number }>
 local colors = {
-  [ 0] = { gui = '#2d2d2d', cterm =  0, r = 0x2d, g = 0x2d, b = 0x2d },
-  [ 1] = { gui = '#393939', cterm = 18, r = 0x39, g = 0x39, b = 0x39 },
-  [ 2] = { gui = '#515151', cterm = 19, r = 0x51, g = 0x51, b = 0x51 },
-  [ 3] = { gui = '#747369', cterm =  8, r = 0x74, g = 0x73, b = 0x69 },
-  [ 4] = { gui = '#a09f93', cterm = 20, r = 0xa0, g = 0x9f, b = 0x93 },
-  [ 5] = { gui = '#d3d0c8', cterm =  7, r = 0xd3, g = 0xd0, b = 0xc8 },
-  [ 6] = { gui = '#e8e6df', cterm = 21, r = 0xe8, g = 0xe6, b = 0xdf },
-  [ 7] = { gui = '#f2f0ec', cterm = 15, r = 0xf2, g = 0xf0, b = 0xec },
-  [ 8] = { gui = '#f2777a', cterm =  1, r = 0xf2, g = 0x77, b = 0x7a },
-  [ 9] = { gui = '#f99157', cterm = 16, r = 0xf9, g = 0x91, b = 0x57 },
-  [10] = { gui = '#ffcc66', cterm =  3, r = 0xff, g = 0xcc, b = 0x66 },
-  [11] = { gui = '#99cc99', cterm =  2, r = 0x99, g = 0xcc, b = 0x99 },
-  [12] = { gui = '#66cccc', cterm =  6, r = 0x66, g = 0xcc, b = 0xcc },
-  [13] = { gui = '#6699cc', cterm =  4, r = 0x66, g = 0x99, b = 0xcc },
-  [14] = { gui = '#cc99cc', cterm =  5, r = 0xcc, g = 0x99, b = 0xcc },
-  [15] = { gui = '#d27b53', cterm = 17, r = 0xd2, g = 0x7b, b = 0x53 },
+  [ 0] = { gui = 0x2d2d2d, cterm =  0, r = 0x2d, g = 0x2d, b = 0x2d },
+  [ 1] = { gui = 0x393939, cterm = 18, r = 0x39, g = 0x39, b = 0x39 },
+  [ 2] = { gui = 0x515151, cterm = 19, r = 0x51, g = 0x51, b = 0x51 },
+  [ 3] = { gui = 0x747369, cterm =  8, r = 0x74, g = 0x73, b = 0x69 },
+  [ 4] = { gui = 0xa09f93, cterm = 20, r = 0xa0, g = 0x9f, b = 0x93 },
+  [ 5] = { gui = 0xd3d0c8, cterm =  7, r = 0xd3, g = 0xd0, b = 0xc8 },
+  [ 6] = { gui = 0xe8e6df, cterm = 21, r = 0xe8, g = 0xe6, b = 0xdf },
+  [ 7] = { gui = 0xf2f0ec, cterm = 15, r = 0xf2, g = 0xf0, b = 0xec },
+  [ 8] = { gui = 0xf2777a, cterm =  1, r = 0xf2, g = 0x77, b = 0x7a },
+  [ 9] = { gui = 0xf99157, cterm = 16, r = 0xf9, g = 0x91, b = 0x57 },
+  [10] = { gui = 0xffcc66, cterm =  3, r = 0xff, g = 0xcc, b = 0x66 },
+  [11] = { gui = 0x99cc99, cterm =  2, r = 0x99, g = 0xcc, b = 0x99 },
+  [12] = { gui = 0x66cccc, cterm =  6, r = 0x66, g = 0xcc, b = 0xcc },
+  [13] = { gui = 0x6699cc, cterm =  4, r = 0x66, g = 0x99, b = 0xcc },
+  [14] = { gui = 0xcc99cc, cterm =  5, r = 0xcc, g = 0x99, b = 0xcc },
+  [15] = { gui = 0xd27b53, cterm = 17, r = 0xd2, g = 0x7b, b = 0x53 },
 }
 theme.base16_colors = colors
 theme.ansi_colors = {
