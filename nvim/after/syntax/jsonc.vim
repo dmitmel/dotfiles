9fcaa675af9e83ddfb80c11cdb3842972e2a9ba4
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2025-07-18 03:20:43 +0200

    [nvim] remove highlighting of comments from the regular JSON filetype

--- nvim/after/syntax/jsonc.vim
+++ nvim/after/syntax/jsonc.vim
@@ -2,3 +2,8 @@ if hlexists('jsonCommentTodo')
   syn clear jsonCommentTodo
   execute 'syn match jsonCommentTodo contained' dotfiles#todo_comments#get_pattern()
 endif
+
+if get(g:, 'vim_json_warnings', 1)
+  syn clear jsonCommentError
+  syn clear jsonTrailingCommaError
+endif
--- nvim/ftdetect/dotfiles.lua
+++ nvim/ftdetect/dotfiles.lua
@@ -1,3 +1,5 @@
+if not vim.filetype then return end
+
 vim.filetype.add({
   extension = {
     log = 'log',
@@ -8,11 +10,14 @@ vim.filetype.add({
     ['.vimspector.json'] = 'jsonc',
     ['.latexmkrc'] = 'perl',
     ['coc-settings.json'] = 'jsonc',
+    ['pyrightconfig.json'] = 'jsonc',
+    ['.luarc.json'] = 'jsonc',
   },
   pattern = {
     ['.*/assets/.*%.json%.patch'] = 'json',
     ['.*/etc/fonts/.*%.conf'] = 'xml',
     ['.*/fontconfig/.*%.conf'] = 'xml',
     ['.*/snippets/.*%.json'] = 'jsonc',
+    ['.*/%.vscode/.*%.json'] = 'jsonc',
   },
 })
