81ae8ebeece4391aedaff28e809dd4fa86b5aac6
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2025-10-31 21:06:17 +0100

    [zsh] refactor prompt.zsh

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,42 +1,58 @@
 #!/usr/bin/env zsh
 
-# NOTE: This script must be loaded after `alias_tips.zsh` and `plugins.zsh`, so
-# that the precmd hook is added last. This has to be done so that this script
-# can get an accurate assesment of the command run-time.
+zmodload zsh/datetime  # for $EPOCHREALTIME
+autoload -Uz add-zsh-hook
 
-# Escapes `%` in all arguments by replacing it with `%%`. Escaping is needed so
-# that untrusted input (e.g. git branch names) doesn't affect prompt rendering.
-prompt_escape() {
-  print -rn -- "${@//\%/%%}"
+# The installation of hooks for measuring command execution time is deferred
+# until after the shell is initialized and the first prompt is drawn. This is
+# done so that this script can control the order of placement of its hooks, so
+# that it doesn't depend on the load order of other scripts and plugins.
+add-zsh-hook precmd _prompt_install_hooks
+_prompt_install_hooks() {
+  add-zsh-hook -d preexec _prompt_install_hooks  # uninstall this temporary hook
+  unfunction _prompt_install_hooks               # and unload it from memory
+
+  # I manipulate the `*_functions` arrays directly here because `add-zsh-hook`
+  # does not let me control the placement order. To get the most accurate
+  # assessment of the run-time of a command, we must achieve the following
+  # sequence of events:
+  #
+  # 1. user types in a command and presses <Enter>
+  # 2. preexec hooks are executed
+  # 3. **prompt_preexec_hook is executed and records the start time**
+  # 4. ...the command runs...
+  # 5. the command exits
+  # 6. **prompt_precmd_hook is executed and records the stop time**
+  # 7. all other precmd hooks are executed
+  # 8. a new prompt is drawn
+  typeset -ag preexec_functions=("${preexec_functions[@]:#prompt_preexec_hook}" prompt_preexec_hook)
+  typeset -ag precmd_functions=(prompt_precmd_hook "${precmd_functions[@]:#prompt_precmd_hook}")
 }
 
 prompt_preexec_hook() {
-  # record command start time
-  # $EPOCHREALTIME returns a float representing system time in seconds (see
-  # docs for zsh/datetime module) and is much faster than `date +%s.%N` because
-  # it doesn't involve starting a process
-  typeset -gF _PROMPT_EXEC_START_TIME="$EPOCHREALTIME"
+  # $EPOCHREALTIME returns a float representing the system time in seconds (see
+  # docs for the zsh/datetime module). It is much faster than `date +%s.%N`
+  # because it doesn't involve forking another process.
+  float -g _PROMPT_EXEC_START_TIME="$EPOCHREALTIME"
 }
 
 prompt_precmd_hook() {
   if [[ -v _PROMPT_EXEC_START_TIME ]]; then
-    local -F duration="$((EPOCHREALTIME - _PROMPT_EXEC_START_TIME))"
+    float t=$(( EPOCHREALTIME - _PROMPT_EXEC_START_TIME ))
     unset _PROMPT_EXEC_START_TIME
 
-    if (( duration > 1 )); then
-      local -i t="$duration" d h m s
-      typeset -g _PROMPT_EXEC_TIME=""
-      d="$((t/60/60/24))"
-      h="$((t/60/60%24))"
-      m="$((t/60%60))"
-      s="$((t%60))"
+    typeset -g _PROMPT_EXEC_TIME=""
+    if (( t > 1 )); then
+      integer d h m s
+      (( d = t/60/60/24, h = t/60/60%24, m = t/60%60, s = t%60 ))
       (( d > 0 )) && _PROMPT_EXEC_TIME+="${d}d"
       (( h > 0 )) && _PROMPT_EXEC_TIME+="${h}h"
       (( m > 0 )) && _PROMPT_EXEC_TIME+="${m}m"
       _PROMPT_EXEC_TIME+="${s}s"
     else
-      local -i t="$((duration*1000))"
-      _PROMPT_EXEC_TIME="${t}ms"
+      integer ms
+      (( ms = t * 1000 ))
+      _PROMPT_EXEC_TIME="${ms}ms"
     fi
   fi
 }
@@ -56,7 +72,9 @@ prompt_vcs_info() {
     fi
   done
 
-  print -rn -- ' %F{blue}git:%F{magenta}'"$(prompt_escape "$branch")"'%f'
+  # Be sure to escape `%` in the branch name by replacing them with `%%`, so
+  # that untrusted input does not affect prompt rendering.
+  print -rn -- " %F{blue}git:%F{magenta}${branch//\%/%%}%f"
 }
 
 # configure prompt expansion
@@ -71,11 +89,6 @@ prompt_vcs_info() {
 # promptsubst
 #   enable parameter/command/arithmetic expansion/substitution in the prompt.
 setopt no_prompt_bang prompt_cr prompt_sp prompt_percent prompt_subst
-
-zmodload zsh/datetime
-autoload -Uz add-zsh-hook
-add-zsh-hook preexec prompt_preexec_hook
-add-zsh-hook precmd prompt_precmd_hook
 
 # Construct the prompt. See EXPANSION OF PROMPT SEQUENCES in zshmisc(1) for
 # the list and descriptions of the expansion sequences.
@@ -107,17 +120,17 @@ PROMPT+=' [%F{red}%D %*%f]'
 PROMPT+='$(prompt_vcs_info 2>/dev/null)'
 
 # Python's virtualenv
-PROMPT+='${VIRTUAL_ENV:+" %F{blue}venv:%F{magenta}$(prompt_escape "${VIRTUAL_ENV:t}")%f"}'
+PROMPT+='${VIRTUAL_ENV:+" %F{blue}venv:%F{magenta}${VIRTUAL_ENV:t//\%/%%}%f"}'
 VIRTUAL_ENV_DISABLE_PROMPT=true
 
 # pyenv
-PROMPT+='${PYENV_VERSION:+" %F{blue}pyenv:%F{magenta}$(prompt_escape "$PYENV_VERSION")%f"}'
+PROMPT+='${PYENV_VERSION:+" %F{blue}pyenv:%F{magenta}${PYENV_VERSION//\%/%%}%f"}'
 PYENV_VIRTUAL_ENV_DISABLE_PROMPT=true
 
 PROMPT+=' '
 
 # command execution time
-PROMPT+='${_PROMPT_EXEC_TIME:+" %F{yellow}$(prompt_escape "$_PROMPT_EXEC_TIME")%f"}'
+PROMPT+='${_PROMPT_EXEC_TIME:+" %F{yellow}${_PROMPT_EXEC_TIME//\%/%%}%f"}'
 
 # exit code of the previous command
 PROMPT+='%(?.. %F{red}EXIT:%?%f)'
