00985e8ac76c2d1ea10700b3f463269366a04cb7
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2025-11-07 04:09:01 +0100

    [zsh] improve documentation of history-related options, load options.zsh as the very first thing

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -31,30 +31,34 @@ setopt auto_pushd pushd_ignore_dups
 
 # do not autoselect the first completion entry
 setopt no_menu_complete
-# if the cursor is inside a word, use part from the beginning to the cursor as
-# prefix and from the cursor to the end as suffix when searching for
+# if the cursor is inside a word, use the part from the beginning to the cursor
+# as prefix and from the cursor to the end as suffix when searching for
 # completions (the default behavior is to use the whole word as a substring)
 setopt complete_in_word
-# setopt always_to_end     # does this option affect anything?
 
-# strangely enough, Zsh doesn't save command history by default
-HISTFILE="${HISTFILE:-$HOME/.zsh_history}"
-# max number of entries stored in memory
-HISTSIZE=210000
-# max number of entries in the HISTFILE
-SAVEHIST=200000
-# record timestamps in the history
+# record timestamps in the history file
 setopt extended_history
-# delete duplicates first when HISTFILE size exceeds HISTSIZE
+# append entries to the HISTFILE immediately after a command gets entered, and
+# synchronize the history between active Zsh sessions
+setopt share_history hist_fcntl_lock
+# delete duplicates first when HISTFILE size exceeds HISTSIZE (unrelated to hist_ignore_dups!)
 setopt hist_expire_dups_first
-# ignore duplicated history items
+# don't add a command to the history if it is the same as the command entered
+# right before it (like, if you press <Up> and <Enter>)
 setopt hist_ignore_dups
-# ignore commands that start with space
+# ignore commands that start with a space character (sort of an incognito mode for the shell)
 setopt hist_ignore_space
-# don't run commands with history expansions immediately, instead allow the
-# user to preview the expansions and edit the command after expansions in ZLE
+# trim ALL unnecessary whitespace from commands saved to the history
+setopt hist_reduce_blanks
+# if a command was entered that includes a history expansion (`!!`, `!$` etc),
+# don't run it immediately, instead, allow the user to preview the expanded
+# result and edit the command if necessary
 setopt hist_verify
-# immediately write HISTFILE to disk when a new command is appended
-setopt inc_append_history
-# synchronize history between active sessions
-setopt share_history
+
+# max number of entries stored in memory
+HISTSIZE=1000000
+# max number of entries in the HISTFILE
+SAVEHIST=1000000
+# set the history file only AFTER all of the history-related options have been
+# configured to avoid corrupting it
+HISTFILE="${HOME}/.zsh_history"
