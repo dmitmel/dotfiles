9a56239ae197e53ab5881f452057e089911869b4
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2025-12-15 13:32:10 +0100

    make some small random improvements to different scripts

--- nvim/vscode2snipmate.py
+++ nvim/vscode2snipmate.py
@@ -1,11 +1,16 @@
+#!/usr/bin/env python3
+
 import sys
+
 import json5
+
 
 def main():
   for _, snippet in json5.load(sys.stdin).items():
     print(f"snippet {snippet['prefix']} {snippet.get('description', '')}")
     for line in snippet["body"]:
       print("\t" + line)
+
 
 if __name__ == "__main__":
   main()
